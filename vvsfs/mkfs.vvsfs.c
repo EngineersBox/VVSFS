efae518e796640cdc3f9ecb6cc08a574d53a063a
Daniel Herald <u7480080@anu.edu.au>
2023-10-05 13:50:24 +1100

    Add simple clang-format style rules

--- vvsfs/mkfs.vvsfs.c
+++ vvsfs/mkfs.vvsfs.c
@@ -2,104 +2,100 @@
  *  mkfs.vvsfs - constructs an initial empty file system
  * Eric McCreath 2006 GPL
  * Alwen Tiu 2023 GPL
- * 
+ *
  */
 
+#include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+
 #include "vvsfs.h"
 
-char* device_name;
+char *device_name;
 int device;
 
-static void die(char *mess)
-{
-    fprintf(stderr,"Exit : %s\n",mess);
+static void die(char *mess) {
+    fprintf(stderr, "Exit : %s\n", mess);
     exit(1);
 }
 
-static void usage(void)
-{
-    die("Usage : mkfs.vvsfs <device name>)");
-}
+static void usage(void) { die("Usage : mkfs.vvsfs <device name>)"); }
 
-static void write_disk(off_t *pos, uint8_t *block, off_t size) 
-{
-    if (*pos != lseek(device,*pos,SEEK_SET))
+static void write_disk(off_t *pos, uint8_t *block, off_t size) {
+    if (*pos != lseek(device, *pos, SEEK_SET))
         die("seek set failed 1");
 
-    if(write(device,block, size) != size) 
+    if (write(device, block, size) != size)
         die("write failed 1");
-    *pos+=size; 
+    *pos += size;
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
     int i;
 
-    uint8_t block[VVSFS_BLOCKSIZE]; 
-    uint8_t magic[VVSFS_BLOCKSIZE]; 
+    uint8_t block[VVSFS_BLOCKSIZE];
+    uint8_t magic[VVSFS_BLOCKSIZE];
     uint8_t imap[VVSFS_BLOCKSIZE];
-    uint8_t dmap[VVSFS_DMAP_SIZE]; 
+    uint8_t dmap[VVSFS_DMAP_SIZE];
 
-    struct vvsfs_inode inode; 
+    struct vvsfs_inode inode;
 
-    if (argc != 2) usage();
+    if (argc != 2)
+        usage();
 
     // open the device for reading and writing
     device_name = argv[1];
-    device = open(device_name,O_RDWR);
+    device = open(device_name, O_RDWR);
 
-    off_t pos=0;
-    
-    printf("Writing magic number\n"); 
+    off_t pos = 0;
+
+    printf("Writing magic number\n");
     // set magic number for the first block
-    memset(magic, 0, VVSFS_BLOCKSIZE); 
-    uint32_t *m = (uint32_t *) magic; 
-    *m = VVSFS_MAGIC; 
+    memset(magic, 0, VVSFS_BLOCKSIZE);
+    uint32_t *m = (uint32_t *)magic;
+    *m = VVSFS_MAGIC;
     write_disk(&pos, magic, VVSFS_BLOCKSIZE);
 
-    printf("Writing inode bitmap\n"); 
+    printf("Writing inode bitmap\n");
     // initialise inode map -- mark first block as taken
     memset(imap, 0, VVSFS_BLOCKSIZE);
     imap[0] = 1 << 7;
     write_disk(&pos, imap, VVSFS_BLOCKSIZE);
 
-    printf("Writing data bitmap\n"); 
-    // initialise data blocks map -- mark first block as taken 
-    memset(dmap, 0, VVSFS_DMAP_SIZE); 
-    dmap[0] = 1 << 7; 
+    printf("Writing data bitmap\n");
+    // initialise data blocks map -- mark first block as taken
+    memset(dmap, 0, VVSFS_DMAP_SIZE);
+    dmap[0] = 1 << 7;
     write_disk(&pos, dmap, VVSFS_DMAP_SIZE);
 
-    printf("Writing root inode\n"); 
-    
+    printf("Writing root inode\n");
+
     // Root inode: occupies first data block
-    memset(block, 0, VVSFS_BLOCKSIZE);  
-    for(i=0; i < VVSFS_N_BLOCKS; ++i)
-        inode.i_block[1] = 0; 
-    inode.i_mode = S_IFDIR | S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH | S_IXUSR | S_IXGRP | S_IXOTH;
+    memset(block, 0, VVSFS_BLOCKSIZE);
+    for (i = 0; i < VVSFS_N_BLOCKS; ++i)
+        inode.i_block[1] = 0;
+    inode.i_mode = S_IFDIR | S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP |
+                   S_IWOTH | S_IXUSR | S_IXGRP | S_IXOTH;
     printf("Mode: %d\n", inode.i_mode);
-    inode.i_data_blocks_count = 1; 
+    inode.i_data_blocks_count = 1;
     inode.i_links_count = 1;
     inode.i_size = 0;
-    memcpy(block, &inode, sizeof(struct vvsfs_inode)); 
-    write_disk(&pos, block, VVSFS_BLOCKSIZE); 
+    memcpy(block, &inode, sizeof(struct vvsfs_inode));
+    write_disk(&pos, block, VVSFS_BLOCKSIZE);
 
     // zero remaining blocks
-    printf("Zeroing remaining blocks\n"); 
-    memset(block, 0, VVSFS_BLOCKSIZE); 
+    printf("Zeroing remaining blocks\n");
+    memset(block, 0, VVSFS_BLOCKSIZE);
     for (i = 5; i < VVSFS_MAXBLOCKS; i++)
-        write_disk(&pos, block, VVSFS_BLOCKSIZE); 
+        write_disk(&pos, block, VVSFS_BLOCKSIZE);
 
     close(device);
-    printf("Done\n"); 
+    printf("Done\n");
 
     return 0;
 }
-
--- vvsfs/vvsfs.h
+++ vvsfs/vvsfs.h
@@ -1,20 +1,21 @@
-#define VVSFS_BLOCKSIZE 1024        // filesystem blocksize
-#define VVSFS_SECTORSIZE 512        // disk sector size
-#define VVSFS_INODESIZE 256         // inode size
-#define VVSFS_N_INODE_PER_BLOCK ((VVSFS_BLOCKSIZE/VVSFS_INODESIZE)) // maximum number of inodes per block
-#define VVSFS_N_BLOCKS  15        
+#define VVSFS_BLOCKSIZE 1024 // filesystem blocksize
+#define VVSFS_SECTORSIZE 512 // disk sector size
+#define VVSFS_INODESIZE 256  // inode size
+#define VVSFS_N_INODE_PER_BLOCK                                                \
+    ((VVSFS_BLOCKSIZE / VVSFS_INODESIZE)) // maximum number of inodes per block
+#define VVSFS_N_BLOCKS 15
 #define VVSFS_MAXBLOCKS 20484
 #define VVSFS_IMAP_SIZE 512
 #define VVSFS_DMAP_SIZE 2048
-#define VVSFS_MAGIC     0xCAFEB0BA
-#define VVSFS_INODE_BLOCK_OFF   4       // location of first inode
-#define VVSFS_DATA_BLOCK_OFF    4100    // location of first data block
-#define VVSFS_MAX_DENTRIES  ((VVSFS_N_DENTRY_PER_BLOCK * VVSFS_N_BLOCKS))
+#define VVSFS_MAGIC 0xCAFEB0BA
+#define VVSFS_INODE_BLOCK_OFF 4   // location of first inode
+#define VVSFS_DATA_BLOCK_OFF 4100 // location of first data block
+#define VVSFS_MAX_DENTRIES ((VVSFS_N_DENTRY_PER_BLOCK * VVSFS_N_BLOCKS))
 
 #define VVSFS_MAXFILESIZE ((VVSFS_BLOCKSIZE * VVSFS_N_BLOCKS))
 #define true 1
 #define false 0
-#define MIN(a,b)        (((a)<(b))?(a):(b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 #ifdef __KERNEL__
 #include <linux/types.h>
@@ -26,7 +27,7 @@
 
 super block:    [info               ]          1 block (only 4 bytes used)
                 [inode maps         ]          1 block (only 512 bytes used)
-                [data block maps    ]          2 blocks 
+                [data block maps    ]          2 blocks
 inode table:    [inode table        ]        512*8 blocks
 data blocks:    [data blocks        ]       2048*8 blocks
 
@@ -34,27 +35,28 @@ inode size: 256 bytes
 
 */
 
-struct vvsfs_inode
-{
-    uint32_t i_mode; 
-    uint32_t i_size;   /* Size in bytes */
-    uint32_t i_links_count;  /* Links count */
-    uint32_t i_data_blocks_count; /* Data block counts, for block size VVSFS_BLOCKSIZE. 
-                                     Note that this may not be the same as VFS inode i_blocks member. */
+struct vvsfs_inode {
+    uint32_t i_mode;
+    uint32_t i_size;        /* Size in bytes */
+    uint32_t i_links_count; /* Links count */
+    uint32_t
+        i_data_blocks_count;          /* Data block counts, for block size
+                                         VVSFS_BLOCKSIZE.     Note that this may not be
+                                         the same as VFS inode i_blocks member. */
     uint32_t i_block[VVSFS_N_BLOCKS]; /* Pointers to blocks */
-    
 };
 
-#define VVSFS_MAXNAME   123         // maximum size of filename
+#define VVSFS_MAXNAME 123 // maximum size of filename
 
-struct vvsfs_dir_entry
-{
-    char     name[VVSFS_MAXNAME+1];
+struct vvsfs_dir_entry {
+    char name[VVSFS_MAXNAME + 1];
     uint32_t inode_number;
 };
 
-#define VVSFS_DENTRYSIZE ((sizeof(struct vvsfs_dir_entry)))        
-#define VVSFS_N_DENTRY_PER_BLOCK ((VVSFS_BLOCKSIZE/VVSFS_DENTRYSIZE))   // maximum number of directory entries per block
+#define VVSFS_DENTRYSIZE ((sizeof(struct vvsfs_dir_entry)))
+#define VVSFS_N_DENTRY_PER_BLOCK                                               \
+    ((VVSFS_BLOCKSIZE /                                                        \
+      VVSFS_DENTRYSIZE)) // maximum number of directory entries per block
 
 #ifdef __KERNEL__
 
@@ -62,110 +64,96 @@ struct vvsfs_dir_entry
 // @map:  the bitmap that keeps track of free blocks
 // @size: the size of the bitmap.
 //
-// This function finds the first free block in a bitmap and toggles the corresponding bit 
-// in the bitmap. Note that block 0 is always reserved, so the search always start from 1. 
-// On success the position of the first free block is returned, and the bitmap is updated. 
-// On failure, the function returns 0 and leave the bitmap unchanged.
-// NOTE: the position returned is relative to the start of the map, so it is *not* the actual
-// location on disk. 
+// This function finds the first free block in a bitmap and toggles the
+// corresponding bit in the bitmap. Note that block 0 is always reserved, so the
+// search always start from 1. On success the position of the first free block
+// is returned, and the bitmap is updated. On failure, the function returns 0
+// and leave the bitmap unchanged. NOTE: the position returned is relative to
+// the start of the map, so it is *not* the actual location on disk.
 
-static uint32_t vvsfs_find_free_block(uint8_t * map, uint32_t size)
-{
-    int i=0;
-    uint8_t j=0;
+static uint32_t vvsfs_find_free_block(uint8_t *map, uint32_t size) {
+    int i = 0;
+    uint8_t j = 0;
 
-    for(i=0; i < size; ++i)
-    {
-        for(j=0; j < 8; ++j)
-        {
-            if(i==0 && j==0) continue; // skip block 0 -- it is reserved. 
-            if((~map[i]) & (0x80 >> j))
-            {
-                map[i] = map[i] | (0x80 >> j); 
-                return i*8 + j; 
+    for (i = 0; i < size; ++i) {
+        for (j = 0; j < 8; ++j) {
+            if (i == 0 && j == 0)
+                continue; // skip block 0 -- it is reserved.
+            if ((~map[i]) & (0x80 >> j)) {
+                map[i] = map[i] | (0x80 >> j);
+                return i * 8 + j;
             }
         }
     }
     return 0;
 }
 
-static void vvsfs_free_block(uint8_t * map, uint32_t pos)
-{
-    uint32_t i=pos/8; 
-    uint8_t j = pos % 8; 
+static void vvsfs_free_block(uint8_t *map, uint32_t pos) {
+    uint32_t i = pos / 8;
+    uint8_t j = pos % 8;
 
     map[i] = map[i] & ~(0x80 >> j);
-
 }
 
 // mapping from position in an imap to inode number and vice versa.
 // 0 is an invalid inode number
-#define BNO_TO_INO(x)   ((x+1))
-#define INO_TO_BNO(x)   ((x-1))
-#define BAD_INO(x)      ((x==0))
-
+#define BNO_TO_INO(x) ((x + 1))
+#define INO_TO_BNO(x) ((x - 1))
+#define BAD_INO(x) ((x == 0))
 
 // vvsfs_reserve_inode_block
-// @map: a bitmap representing inode blocks on disk 
-// 
-// On success, this function returns a valid inode number and update the corresponding 
-// inode bitmap. On failure, it will return 0 (which is an invalid inode number),
-// and leave the bitmap unchanged. 
-static uint32_t vvsfs_reserve_inode_block(uint8_t * map)
-{
-    uint32_t i = vvsfs_find_free_block(map, VVSFS_IMAP_SIZE); 
-    if(i == 0) return 0;
-    return BNO_TO_INO(i); 
+// @map: a bitmap representing inode blocks on disk
+//
+// On success, this function returns a valid inode number and update the
+// corresponding inode bitmap. On failure, it will return 0 (which is an invalid
+// inode number), and leave the bitmap unchanged.
+static uint32_t vvsfs_reserve_inode_block(uint8_t *map) {
+    uint32_t i = vvsfs_find_free_block(map, VVSFS_IMAP_SIZE);
+    if (i == 0)
+        return 0;
+    return BNO_TO_INO(i);
 }
 
-static void vvsfs_free_inode_block(uint8_t * map, uint32_t ino)
-{
-    vvsfs_free_block(map, INO_TO_BNO(ino)); 
+static void vvsfs_free_inode_block(uint8_t *map, uint32_t ino) {
+    vvsfs_free_block(map, INO_TO_BNO(ino));
 }
 
-static uint32_t vvsfs_reserve_data_block(uint8_t * map)
-{
-    return vvsfs_find_free_block(map, VVSFS_DMAP_SIZE); 
+static uint32_t vvsfs_reserve_data_block(uint8_t *map) {
+    return vvsfs_find_free_block(map, VVSFS_DMAP_SIZE);
 }
 
-static void vvsfs_free_data_block(uint8_t * map, uint32_t dno)
-{
-    vvsfs_free_block(map, dno); 
+static void vvsfs_free_data_block(uint8_t *map, uint32_t dno) {
+    vvsfs_free_block(map, dno);
 }
 
 // get the disk block number for a given inode number
-uint32_t vvsfs_get_inode_block(unsigned long ino)
-{
-    return (VVSFS_INODE_BLOCK_OFF + INO_TO_BNO(ino)/VVSFS_N_INODE_PER_BLOCK);
+uint32_t vvsfs_get_inode_block(unsigned long ino) {
+    return (VVSFS_INODE_BLOCK_OFF + INO_TO_BNO(ino) / VVSFS_N_INODE_PER_BLOCK);
 }
 
-// inode offset (in bytes) relative to the start of the block 
-uint32_t vvsfs_get_inode_offset(unsigned long ino)
-{
-    return ( INO_TO_BNO(ino) % VVSFS_N_INODE_PER_BLOCK) * VVSFS_INODESIZE;
+// inode offset (in bytes) relative to the start of the block
+uint32_t vvsfs_get_inode_offset(unsigned long ino) {
+    return (INO_TO_BNO(ino) % VVSFS_N_INODE_PER_BLOCK) * VVSFS_INODESIZE;
 }
 
 // get the disk block number for a given logical data block number
-uint32_t vvsfs_get_data_block(uint32_t bno)
-{
+uint32_t vvsfs_get_data_block(uint32_t bno) {
     return VVSFS_DATA_BLOCK_OFF + bno;
 }
 
-// A macro to extract a vvsfs_inode_info object from a VFS inode. 
+// A macro to extract a vvsfs_inode_info object from a VFS inode.
 #define VVSFS_I(inode) (container_of(inode, struct vvsfs_inode_info, vfs_inode))
 
-// A "container" structure that keeps the VFS inode and additional on-disk data. 
-struct vvsfs_inode_info
-{
-    uint32_t i_db_count; /* Data blocks count */ 
+// A "container" structure that keeps the VFS inode and additional on-disk data.
+struct vvsfs_inode_info {
+    uint32_t i_db_count;             /* Data blocks count */
     uint32_t i_data[VVSFS_N_BLOCKS]; /* Pointers to blocks */
-    struct inode vfs_inode; 
+    struct inode vfs_inode;
 };
 
-struct vvsfs_sb_info 
-{
-    uint8_t * imap; /* inode blocks map */
-    uint8_t * dmap; /* data blocks map  */
+struct vvsfs_sb_info {
+    uint8_t *imap; /* inode blocks map */
+    uint8_t *dmap; /* data blocks map  */
 };
 
-#endif 
+#endif
