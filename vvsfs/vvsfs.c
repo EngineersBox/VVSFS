0582595ecc0dc9b66fa05ba6772f9a70d0f05184
EngineersBox <toastcraft.info@gmail.com>
2023-10-16 23:26:04 +1100

    Added normal logging macro and updated DEBUG_LOG to have [DEBUG] prefix

--- vvsfs/vvsfs.c
+++ vvsfs/vvsfs.c
@@ -28,8 +28,15 @@
 #define DEBUG 1
 #define LOG_FILE_PATH 0
 
+#define __LOG(prefix, msg, ...)                                                \
+    printk(prefix "%s(" FILE_FORMAT_PARAMETER "%d) :: " msg,                   \
+           __func__,                                                           \
+           FILE_MARKER __LINE__,                                               \
+           ##__VA_ARGS__)
+#define LOG(msg, ...) __LOG("", msg, __VA_ARGS__)
+
 #if defined(LOG_FILE_PATH) && LOG_FILE_PATH == 1
-#define FILE_FORMAT_PARAMETER "%s"
+#define FILE_FORMAT_PARAMETER "%s:"
 #define FILE_MARKER __FILE__
 #else
 #define FILE_FORMAT_PARAMETER ""
@@ -37,11 +44,7 @@
 #endif
 
 #if defined(DEBUG) && DEBUG == 1
-#define DEBUG_LOG(msg, ...)                                                    \
-    printk("%s(" FILE_FORMAT_PARAMETER ":%d) :: " msg,                         \
-           __func__,                                                           \
-           FILE_MARKER __LINE__,                                               \
-           ##__VA_ARGS__)
+#define DEBUG_LOG(msg, ...) __LOG("[DEBUG] ", msg, __VA_ARGS__)
 #else
 #define DEBUG_LOG(msg, ...) ({})
 #endif
