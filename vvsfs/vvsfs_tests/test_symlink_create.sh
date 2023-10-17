dfc4ad4a1aefb5bee9f89c88fd61db0b91f7d6a1
Daniel Herald <u7480080@anu.edu.au>
2023-10-17 12:11:51 +1100

    Implement symlinks

---
+++ vvsfs/vvsfs_tests/test_symlink_create.sh
@@ -0,0 +1,25 @@
+source ./assert.sh
+
+log_header "Testing symlink create"
+
+./create.sh
+
+content="ABCDEFG"
+
+echo "blah" > testdir/aaa
+ln -s aaa testdir/bbb
+ln -s aaa testdir/ccc
+ln -s ccc testdir/ddd
+echo "$content" > testdir/bbb
+
+./remount.sh
+
+assert_eq "$content" "$(cat testdir/aaa)" "expected correct content"
+assert_eq "$content" "$(cat testdir/bbb)" "expected correct content"
+assert_eq "$content" "$(cat testdir/ccc)" "expected correct content"
+assert_eq "$content" "$(cat testdir/ddd)" "expected correct content"
+
+assert_eq "'testdir/aaa'" "$(stat testdir/aaa -c %N)" "expected no symlink"
+assert_eq "'testdir/bbb' -> 'aaa'" "$(stat testdir/bbb -c %N)" "expected link to point correctly"
+assert_eq "'testdir/ccc' -> 'aaa'" "$(stat testdir/ccc -c %N)" "expected link to point correctly"
+assert_eq "'testdir/ddd' -> 'ccc'" "$(stat testdir/ddd -c %N)" "expected link to point correctly"
