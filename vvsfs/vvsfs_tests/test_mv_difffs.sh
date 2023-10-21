158aec97cd69f22bd959bdc0ef6bd6b4b6f4e468
Angus Atkinson <u7117106@anu.edu.au>
2023-10-21 04:46:09 +0000

    Add success log messages to rename tests

--- vvsfs/vvsfs_tests/test_mv_difffs.sh
+++ vvsfs/vvsfs_tests/test_mv_difffs.sh
@@ -14,7 +14,9 @@ mv testdir/filea testdir-otherfs/filea2
 mv testdir-otherfs/fileb testdir/fileb2
 
 assert_eq "$(ls testdir)" "fileb2" "File was not moved into vvsfs (in memory)"
+check_log_success "Move file into VVSFS (create file)"
 assert_eq "$(ls testdir-otherfs)" "filea2" "File was not moved out of vvsfs (in memory)"
+check_log_success "Move file out of VVSFS (unlink file)"
 
 
 ./umount.sh
@@ -22,5 +24,6 @@ assert_eq "$(ls testdir-otherfs)" "filea2" "File was not moved out of vvsfs (in 
 
 assert_eq "$(ls testdir)" "fileb2" "File was not moved into vvsfs (on disk)"
 assert_eq "$(ls testdir-otherfs)" "filea2" "File was not moved out of vvsfs (on disk)"
+check_log_success "All renames survived remount"
 
 rm -rf testdir-otherfs
