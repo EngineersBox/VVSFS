d1ed65bbda382f0de9f12c40affeb5b10859d043
EngineersBox <toastcraft.info@gmail.com>
2023-10-22 02:17:59 +1100

    Removed inline modifier on non-inlinable method

--- vvsfs/vvsfs.h
+++ vvsfs/vvsfs.h
@@ -338,7 +338,7 @@ static void vvsfs_free_block(uint8_t *map, uint32_t pos) {
 // On success, this function returns a valid inode number and update the
 // corresponding inode bitmap. On failure, it will return 0 (which is an invalid
 // inode number), and leave the bitmap unchanged.
-static inline uint32_t vvsfs_reserve_inode_block(uint8_t *map) {
+static uint32_t vvsfs_reserve_inode_block(uint8_t *map) {
     uint32_t i = vvsfs_find_free_block(map, VVSFS_IMAP_SIZE);
     if (i == 0)
         return 0;
