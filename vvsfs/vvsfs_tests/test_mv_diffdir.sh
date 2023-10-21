90e8b937f3bca62ec84a6b676bfbafe77c2694cd
Angus Atkinson <u7117106@anu.edu.au>
2023-10-21 04:46:09 +0000

    Add success log messages to rename tests

--- vvsfs/vvsfs_tests/test_mv_diffdir.sh
+++ vvsfs/vvsfs_tests/test_mv_diffdir.sh
@@ -14,8 +14,10 @@ touch testdir/fileb
 mv testdir/fileb testdir/afolder/filec
 
 assert_eq "$(ls -x testdir/afolder)" "filea  filec" "A file was not successfully moved (in memory)"
+check_log_success "File was successfully moved to a different directory"
 
 ./umount.sh
 ./mount.sh
 
 assert_eq "$(ls -x testdir/afolder)" "filea  filec" "A file was not successfully moved (on disk)"
+check_log_success "Rename survived a remount"
--- vvsfs/vvsfs_tests/test_mv_overwrite.sh
+++ vvsfs/vvsfs_tests/test_mv_overwrite.sh
@@ -15,13 +15,17 @@ echo "bfile" > testdir/dstfolder/folder/bfile
 
 assert_eq "$(mv testdir/srcfolder/folder testdir/dstfolder 2>&1)" "mv: cannot move 'testdir/srcfolder/folder' to 'testdir/dstfolder/folder': Directory not empty" "mv should generate error when moving to nonempty dir"
 assert_eq "$(ls -x testdir/dstfolder/folder)" "bfile" "Should not overwrite a folder which contains files"
+check_log_success "Renaming a folder to a non-empty destination folder should fail"
 assert_eq "$(ls -x testdir/srcfolder)" "folder" "Should not delete folder on failed move"
 assert_eq "$(ls -x testdir/srcfolder/folder)" "afile" "Should not delete folder contents on failed move"
+check_log_success "A failed move should not delete the original source file"
 
 rm testdir/dstfolder/folder/bfile
 mv testdir/srcfolder/folder testdir/dstfolder
 assert_eq "$(ls -x testdir/dstfolder/folder)" "afile" "Should overwrite an empty folder (in memory)"
+check_log_success "Should be possible to overwrite an empty folder"
 assert_eq "$(ls -x testdir/srcfolder)" "" "Should delete folder on successful move (in memory)"
+check_log_success "After a successful move, the src folder should be deleted"
 
 # Adding this back in fixes the test
 # sync
@@ -33,6 +37,7 @@ echo "aaa" > testdir/files/a
 echo "bbb" > testdir/files/b
 mv testdir/files/a testdir/files/b
 assert_eq "$(<testdir/files/b)" "aaa" "Files should overwrite each other (in memory)"
+check_log_success "Files should be able to overwrite files"
 
 ./remount.sh
 
@@ -40,3 +45,5 @@ assert_eq "$(ls -x testdir/dstfolder/folder)" "afile" "Should overwrite an empty
 assert_eq "$(ls -x testdir/srcfolder)" "" "Should delete folder on successful move (on disk)"
 
 # assert_eq "$(<testdir/files/b)" "aaa" "Files should overwrite each other (on disk)"
+
+check_log_success "All renames survive a remount"
