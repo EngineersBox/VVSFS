0816b58415045815508c5ef8460edc1283d1b977
EngineersBox <toastcraft.info@gmail.com>
2023-10-22 02:13:23 +1100

    Added empty define extraction condition to sed expressions, added explanations and fixed test_statfs success logs

--- vvsfs/vvsfs_tests/generate_env.sh
+++ vvsfs/vvsfs_tests/generate_env.sh
@@ -16,9 +16,25 @@ echo " - VVSFS_HEADER = $VVSFS_HEADER"
 echo " - CC = $CC"
 echo
 
+# Sed lines explanation:
+# 1. Keeps lines prefixed with "#define"
+# 2. Removes empty definition line "#define VVSFS_H"
+#    keeping normal definitions like "#define true 1"
+# 3. Extract name of define and replace entire
+#    expression of "#define <NAME> <VALUE>" to
+#    "<NAME>"
+# Note that we use the -E and -dM flags with the
+# compiler to perform the following:
+# -E: Do a pre-process pass only
+# -dM: Dump macro expressions to stdout (note that
+#      without -E, this is interpreted instead as
+#      -fdump-rtl-mach, used to dump compiler state
+#      after the machine dependent reorganisation
+#      pass)
 DEFINES_NAMES=$(
     sed -nr '{
         /^#define/!d
+        /^#define[ \t]*[^ \t]*[ \t]*$/d
         s/^#define[ \t](VVSFS[^ \t(]*)([ \t]\(?).*/\1/p
     }' <($CC -E -dM $VVSFS_HEADER)
 )
@@ -27,6 +43,29 @@ EXPANSION_CODE=$(
     echo "$DEFINES_NAMES" \
     | awk '{print "EXPAND(\""$0"\","$0");"}'
 )
+
+# Code explanation:
+#
+# The format_specifier macro uses the builtin
+# compile time _Generic(x) to extract the type
+# name from the expression, then pattern matches
+# this against the type expressions to convert
+# the appropriate type to a printf format
+# specifier. This allows for fully type-safe
+# printing of resolved values (expressions).
+#
+# The EXPAND macro uses the builtin compile time
+# __typeof__ expression to fully qualify the
+# definition of the temporary _a according to the
+# type of the expression passed in (this is
+# different from _Generic(x) which is a pattern
+# matching expansion and token consumer). Using
+# this (now type safe) declaration of _a, we can
+# print its value according to the appropriate
+# format specifier resolved with the previously
+# explained macro format_specifier along with the
+# name, into a bash variable delcaration format
+# matching <NAME>=<VALUE>.
 
 # Note the use of "|| true" here, since "read"
 # returns an exit code of 1, we need to ensure
@@ -66,6 +105,7 @@ EOF
 TMP=$(mktemp)
 echo "$EXPANSION_CODE" | gcc -o $TMP -xc -
 chmod a+x $TMP
+
 echo "#!/bin/bash" > $ENV_FILE_NAME
 $TMP >> $ENV_FILE_NAME
 chmod a+x $ENV_FILE_NAME
